57cf616624f3ad261735ac7ea131c9999de63045
iknowright <changchaishi@gmail.com>
2019-12-31 14:22:56 +0800

    add youtube module and youtube then small edit on research

--- app/src/main/java/com/example/finalproject/search.java
+++ app/src/main/java/com/example/finalproject/search.java
@@ -3,12 +3,30 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class search extends AppCompatActivity {
+
+    private Button btnSuggest;
+    private Button btnNormal;
+    private Button btnAudio;
+    private String name;
+    private int age;
+    private String injury_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        setInit();
+    }
+
+    private void setInit() {
+        btnSuggest = findViewById(R.id.btnSuggestSearch);
+        btnNormal = findViewById(R.id.btnNormalSearch);
+        btnAudio = findViewById(R.id.btnAudioSearch);
+
+
     }
 }
